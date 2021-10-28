a1c166533ed895d20aab3174e677df701313f43e
Cameron <camcreel@gmail.com>
2021-10-28 19:28:18 -0400

    refactor same function

--- frequency-counter.js
+++ frequency-counter.js
@@ -21,8 +21,32 @@ function sameRefactored(arr1,arr2) {
 
     let frequencyCounter1 = {}
     let frequencyCounter2 = {}
-    
+
+    for (let val of arr1) {
+        frequencyCounter1[val] = (frequencyCounter1[val] || 0) + 1
+    }
+
+    for (let val of arr2) {
+        frequencyCounter2[val] = (frequencyCounter2[val] || 0) + 1
+    }
+
+    for (let key in frequencyCounter1) {
+        if (!(key ** 2 in frequencyCounter2)) {
+            return false
+        }
+        if (!(frequencyCounter1[key] === frequencyCounter2[key ** 2])) {
+            return false
+        }
+
+        
+    }
+
+    return true
+
 }
 
 console.log(same([1,2,3],[1,9,4]))//true
 console.log(same([1,2,5],[1,10,4]))//false
+
+console.log(sameRefactored([1,2,3],[1,9,4]))//true
+console.log(sameRefactored([1,2,5],[1,10,4]))//false
