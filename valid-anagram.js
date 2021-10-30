4e4736e2ef22179791a6b04b7d50ed3a240091af
Cameron <camcreel@gmail.com>
2021-10-29 20:33:32 -0400

    add another way to do anagram check

--- valid-anagram.js
+++ valid-anagram.js
@@ -31,6 +31,39 @@ function validAnagram(word1,word2){
     return true;
   }
 
+  function validAnagram2(word1,word2){
+
+    if(word1.length !== word2.length) {
+        return false;
+    }
+
+    const lookup = {}
+    for (let char of word1) {
+        lookup[char] = lookup[char] ? lookup[char] + 1 : 1;
+    }
+
+    for (let char of word2) {
+        if (!lookup[char]) {//if lookup[char] = 0 or is undefined it is falsy and not an anagram
+            return false;
+        }
+        else {
+            lookup[char] -= 1;
+        }
+        
+
+    }
+
+    return true;
+  }
+
+  console.log("anagram 1")
   console.log(validAnagram("",""))//true
   console.log(validAnagram("cinema","iceman"))//true
   console.log(validAnagram("aaz","zza"))//false
+
+  console.log("anagram 2")
+  console.log(validAnagram2("",""))//true
+  console.log(validAnagram2("cinema","iceman"))//true
+  console.log(validAnagram2("aaz","zza"))//false
+  console.log(validAnagram2("anagram","nagaram"))//true
+  console.log(validAnagram2("papac","caapc"))//false
