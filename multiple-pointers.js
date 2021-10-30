626b6fe3a2d93d95aa1c8aca79967c95987c62a9
Cameron <camcreel@gmail.com>
2021-10-30 16:05:16 -0400

    Count unique values my solution

--- multiple-pointers.js
+++ multiple-pointers.js
@@ -19,3 +19,18 @@ function sumZero(arr) {
 }
 
 console.log(sumZero([-4,-3,-2,-1,0,1,2,3,10])) //[-3,3]
+
+//Count Unique values - my solution
+function countUniqueValues(arr){
+    if (arr.length === 0) return 0;
+    let i=0;
+    for (let j=1;j < arr.length;j++) {
+        if (arr[j] !== arr[i]) {
+            i++;
+            arr[i] = arr[j];
+        }
+    }
+    
+    return i+1;//add the one for the first value
+}
+
