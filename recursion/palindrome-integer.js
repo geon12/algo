1dcbadede156691549064708a99b2f164b8430ac
Cameron <camcreel@gmail.com>
2021-11-10 19:25:53 -0500

    Palindrome integer with recursion

---
+++ recursion/palindrome-integer.js
@@ -0,0 +1,22 @@
+function isPalindrome(x) {
+    
+    if (typeof x === 'number') {
+      x = x.toString();
+    }
+    
+    if (x.length === 0 || x.length === 1) {
+        return true;
+    }
+    
+    if (x[0] !== x.slice(-1)) {
+        return false;
+    }
+    
+    return isPalindrome(x.substring(1,x.length-1));
+};
+
+console.log(isPalindrome(121)) //true
+console.log(isPalindrome(23456))//false
+console.log(isPalindrome(3443))//true
+console.log(isPalindrome(-10))//false
+console.log(isPalindrome(1000021))//false
