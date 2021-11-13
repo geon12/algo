ac0eae70a4bd8cd392511bd914f7265b1ec581f0
Cameron <camcreel@gmail.com>
2021-11-12 21:11:22 -0500

    collect odd values using pure recursion

--- recursion/collect-odd-values.js
+++ recursion/collect-odd-values.js
@@ -14,4 +14,19 @@ function collectOddValues(input) {
     return oddArr; 
 }
 
-console.log(collectOddValues([1,2,3,4,5,6]))//[1,3,5]
+console.log(collectOddValues([1,2,3,4,5,6]));//[1,3,5]
+console.log(collectOddValues([11,9,4,2,21,7,6]));//[11,9,21,7]
+
+function collectOddValuesPure(arr) {
+    newArr = [];
+    if (arr.length === 0) return newArr;
+
+    if (arr[0] % 2 !== 0) newArr.push(arr[0]);
+
+    newArr = newArr.concat(collectOddValues(arr.slice(1)));
+
+    return newArr;
+}
+
+console.log(collectOddValuesPure([1,2,3,4,5,6]));//[1,3,5]
+console.log(collectOddValuesPure([11,9,4,2,21,7,6]));//[11,9,21,7]
