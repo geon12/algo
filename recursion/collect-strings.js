f3c98a5650973730d6e1f06e7976bbc8367f3371
Cameron <camcreel@gmail.com>
2021-11-17 19:22:26 -0500

    use recursion to find strings in object

---
+++ recursion/collect-strings.js
@@ -0,0 +1,14 @@
+//Use recursion to find strings in object
+function collectStrings(obj) {
+    var stringsArr = [];
+    for(var key in obj) {
+        if(typeof obj[key] === 'string') {
+            stringsArr.push(obj[key]);
+        }
+        else if(typeof obj[key] === 'object') {
+            stringsArr = stringsArr.concat(collectStrings(obj[key]));
+        }
+    }
+ 
+    return stringsArr;
+}
